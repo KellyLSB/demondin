8236154d29042b3f4a084662ec9871501dcbe359
Kelly Lauren Summer Becker-Neuding <kbecker@kellybecker.me>
2019-02-18 15:49:58 -0800

    Client integrations; this might be a minor headache

--- jsx/admin/main.jsx
+++ jsx/admin/main.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Items from './items'
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import ApolloClient       from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Items              from './items';
 
 import '../../semantic/dist/semantic.min.css';
 
-ReactDOM.render(
-    <React.Fragment>
-        <h1>Hello, world!</h1><br />
-        <Items />
-    </React.Fragment>,
-    document.getElementById('root')
+const client = new ApolloClient({
+  uri: "http://localhost:8080/"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <h1>Hello, world!</h1>
+    <br />
+    <Items />
+  </ApolloProvider>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- main.go
+++ main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/google/uuid"
-	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"gopkg.in/macaron.v1"
   "github.com/go-macaron/session"
-
-	"github.com/KellyLSB/demondin/models"
-	stripeClient "github.com/stripe/stripe-go/client"
+  
+	"github.com/99designs/gqlgen/handler"
+	"github.com/KellyLSB/demondin/graphql"
 )
 
 func main() {
@@ -57,16 +52,8 @@ func main() {
     IDLength:       16,
     // Configuration section name. Default is "session".
     Section:        "session",
-}))
+  }))
 
-	// Handle Database Connections
-	m.Use(dbInit(
-		os.Getenv("POSTGRES_HOSTPORT"), os.Getenv("POSTGRES_DATABASE"),
-		os.Getenv("POSTGRES_USERNAME"), os.Getenv("POSTGRES_PASSWORD"),
-	))
-
-	// Handle StripeAPI
-	m.Map(stripeClient.New(os.Getenv("STRIPE_PRIVATE_KEY"), nil))
 
 	// Handle Templating
 	m.Use(macaron.Renderer(macaron.RenderOptions{
@@ -88,181 +75,28 @@ func main() {
 	// Routes
 	m.Group("/shop", func() {
 		m.Group("/keeper", func() {
-			m.Get("/", func(
-				ctx *macaron.Context,
-				stripe *stripeClient.API,
-			) {
-				balance, _ := stripe.Balance.Get(nil)
-				ctx.JSON(200, balance)
-			})
-
-			m.Get("/charges", func(
-				ctx *macaron.Context,
-				stripe *stripeClient.API,
-			) {
-				charges := stripe.Charges.List(nil)
-				ctx.JSON(200, charges)
-			})
-
-			m.Get("/items?:ext(\\.[\\w]+$)", func(
-			  ctx *macaron.Context,
-			  db *gorm.DB,
-			) {
-				var badges []*models.Item
-
-				db.Table("items").Select("*").Preload("Prices").Find(&badges)
-
-				switch ctx.Params(":ext") {
-				case ".json":
-					ctx.JSON(200, badges)
-				default:
-					ctx.Data["Badges"] = badges
-					ctx.Data["URL"] = ctx.URLFor("post_badge_price", ":item_id", "f31ac18e-0bb1-428e-952f-75cbc5604f3b", ":ext", ".json")
-					ctx.HTML(200, "shop/keeper/items")
-				}
-			})
-			
-			// I don't like the *.* (:path.:ext) routes option;
-			// I'm feeling a little cornered here on that one
-			m.Put("/items/:id([\\w-]+)?:ext(\\.[\\w]+$)", func(
-				ctx *macaron.Context,
-				db *gorm.DB, log *log.Logger,
-			) {
-				var badge models.Item
-				db.Table("items").Select("*").Preload("Prices").
-					Find(&badge, "items.id = ?", ctx.Params(":id"))
-
-				decoder := json.NewDecoder(ctx.Req.Body().ReadCloser())
-				if err := decoder.Decode(&badge); err == io.EOF {
-					panic(err)
-				} else if err != nil {
-					panic(err)
-				}
-
-				db.Save(&badge)
-				
-				switch ctx.Params(":ext") {
-				case ".json":
-					ctx.JSON(200, badge)
-				default:
-					ctx.Data["Badge"] = badge
-					ctx.HTML(200, "shop/keeper/badge")
-				}
-			})
-			
-			m.Post("/items/:item_id([\\w-]+)/prices?:ext(\\.[\\w]+$)", func(
-				ctx *macaron.Context,
-				db *gorm.DB, log *log.Logger,
-			) {
-				var price models.Price
-				
-				decoder := json.NewDecoder(ctx.Req.Body().ReadCloser())
-				if err := decoder.Decode(&price); err == io.EOF {
-					panic(err)
-				} else if err != nil {
-					panic(err)
-				}
-				
-				// Set the ItemID
-				price.ItemID = uuid.MustParse(ctx.Params(":item_id"))
-				
-				log.Println("Created new Record:\n%+v\n", price)
-				db.Save(&price)
-			}).Name("post_badge_price")
-			
-			m.Put("/items/:item([\\w-]+)/prices/:id([\\w-]+)?:ext(\\.[\\w]+$)", func(
-				ctx *macaron.Context,
-				db *gorm.DB, log *log.Logger,
-			) {
-				var price models.Price
-				db.Table("prices").Select("*").
-					Where("prices.item_id = ?", ctx.Params(":item")).
-					Find(&price, "prices.id = ?", ctx.Params(":id"))
-				
-				//JSONDecoder(ctx, &price)
-				decoder := json.NewDecoder(ctx.Req.Body().ReadCloser())
-				if err := decoder.Decode(&price); err == io.EOF {
-					panic(err)
-				} else if err != nil {
-					panic(err)
-				}
-				
-				log.Println("Updated Record:\n%+v\n", price)
-				db.Save(&price)
-				
-				switch ctx.Params(":ext") {
-				case ".json":
-					ctx.JSON(200, price)
-				default:
-					ctx.Data["Price"] = price
-					ctx.HTML(200, "shop/keeper/price")
-				}
+			m.Get("/", func(ctx *macaron.Context) {
+        ctx.HTML(200, "shop/keeper/items")
 			})
 		})
 
-		m.Get("/items?:ext(\\.[\\w]+$)", func(
-		  ctx *macaron.Context,
-		  log *log.Logger, db *gorm.DB,
-		) {
-			var badges []*models.Item
-
-			db.Table("items").Select("*").
-				//Joins("LEFT JOIN prices ON items.id = prices.item_id").
-				Where("items.is_badge AND items.enabled").Preload("Prices",
-				"? BETWEEN prices.valid_after AND prices.valid_before",
-				time.Now(),
-			).Find(&badges)
-
-			switch ctx.Params(":ext") {
-			case ".json":
-				ctx.JSON(200, badges)
-			default:
-				// shop/badges.html template
-				ctx.HTML(200, "shop/items")
-			}
+		m.Get("/", func(ctx *macaron.Context) {
+			ctx.HTML(200, "shop/items")
 		})
-		
-		m.Group("/invoicing", func() {
-		  m.Get(".json", func(
-		    ctx *macaron.Context, log *log.Logger,
-		    sess session.Store, db *gorm.DB,
-		  ) {
-  		  invoicing := new(Invoicing)
-  		  invoicing.LoadSession(sess, db, log)
-  		  invoicing.SaveSession(sess, db, log)
-  		  ctx.JSON(200, invoicing.Invoice)
-		  })
-		
-  		m.Post("/addToCart.json", func(
-  		  ctx *macaron.Context, log *log.Logger,
-  		  sess session.Store, db *gorm.DB,
-  		) {
-  		  invoicing := new(Invoicing)
-  		  invoicing.LoadSession(sess, db, log)
-  		  invoicing.PostAddToCart(ctx, db, log)
-  		  invoicing.SaveSession(sess, db, log)
-  		  ctx.JSON(200, invoicing.Invoice)
-  		})
-	  })
 	})
 
 	m.Get("/", myHandler)
+	
+	// Graphql
+	m.Get("/playground", handler.Playground("GraphQL playground", "/query"))
+	m.Get("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}})))
 
 	// Start our macaron daemon
 	log.Println("Server is running...")
+	log.Printf("connect to http://%s/playground for GraphQL playground", os.Getenv("HOSTPORT"))
 	log.Println(http.ListenAndServe(os.Getenv("HOSTPORT"), m))
 }
 
 func myHandler(ctx *macaron.Context) string {
 	return "the request path is: " + ctx.Req.RequestURI
 }
-
-func JSONDecoder(ctx *macaron.Context, into interface{}) {
-  decoder := json.NewDecoder(ctx.Req.Body().ReadCloser())
-  if err := decoder.Decode(into); err == io.EOF {
-	  panic(err)
-  } else if err != nil {
-	  panic(err)
-  }
-}
-
